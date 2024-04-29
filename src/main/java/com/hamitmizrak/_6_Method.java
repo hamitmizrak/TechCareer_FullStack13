17d20b3566165c3ac1e7548f949c734b474a42aa
Hamit Mizrak <hamitmizrak@gmail.com>
2024-04-29 13:44:33 +0300

    Method

---
+++ src/main/java/com/hamitmizrak/_6_Method.java
@@ -0,0 +1,42 @@
+package com.hamitmizrak;
+
+import java.util.Scanner;
+
+public class _6_Method {
+
+    // 1-) voidli Parametresiz
+    public static void voidliParametresiz(){
+        System.out.println("voidli Parametresiz");
+    }
+
+    // 2-) voidli Parametreli
+    public static void voidliParametreli(int x, int y){
+        System.out.println("voidli Parametreli "+(x+y));
+    }
+
+    // 3-) voidsiz Parametresiz
+    public static int  voidsizParametresiz(){
+        System.out.println("Lütfen bir sayı giriniz");
+        Scanner klavye=new Scanner(System.in);
+        String sNumber=klavye.nextLine();
+        int result=Integer.valueOf(sNumber);
+        return  result;
+    }
+
+    // 4-) voisiz Parametreli
+    public static double  voidsizParametresiz(int x){
+        return  Math.pow(x,2);
+    }
+
+    public static void main(String[] args) {
+        // Method
+        voidliParametresiz();
+        voidliParametreli(4,5);
+
+        //int result=  voidsizParametresiz();
+        //System.out.println("Sonuç: "+result);
+
+        double result2=  voidsizParametresiz(9);
+        System.out.println(result2);
+    }
+}
