1a826bc4a75b94ae9b7f33c1abb596fb3ba4a0d2
Hamit Mizrak <hamitmizrak@gmail.com>
2024-04-29 12:15:39 +0300

    data type

--- src/main/java/com/hamitmizrak/_1_WhatIsJava.java
+++ src/main/java/com/hamitmizrak/_1_WhatIsJava.java
@@ -6,5 +6,31 @@ public class _1_WhatIsJava {
     /* Multiple Comment*/
     public static void main(String[] args) {
         System.out.println("Merhabalar Nasılsınız");
+        // Variable
+
+        // Tam sayılar
+        byte b1=25;
+        System.out.println(b1);
+        short s1=125;
+        int i1=45545;
+        long l1=454455454544545L;
+
+        // Virgüllü sayılar
+        float f2=14.53f;
+        double d2=1121212.45455445;
+
+        char c3='2';
+        boolean b3=3>1;
+        System.out.println(b3);
+
+        // primitive type - Wrapper type
+        int primitive=44;
+        // vermezsiniz=> primitive=null;
+        Integer objectInt=44;
+        objectInt=null;
+        // zero : Dünyaya gelmiş ancak bir etki güçüne sahip değildir.
+        // null : dünyaya gelmemiş
+        // create: Dünyaya gelmiş
+
     }
 }
