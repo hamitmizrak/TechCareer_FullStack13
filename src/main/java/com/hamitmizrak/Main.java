772270b251d08f606146da4abc537d5c3ad1a20b
Hamit Mizrak <hamitmizrak@gmail.com>
2024-04-29 12:05:37 +0300

    Java

--- src/main/java/com/hamitmizrak/Main.java
+++ src/main/java/com/hamitmizrak/Main.java
@@ -7,6 +7,7 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
+        System.out.println("\n");
 
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
--- src/main/java/com/hamitmizrak/_1_WhatIsJava.java
+++ src/main/java/com/hamitmizrak/_1_WhatIsJava.java
@@ -1,2 +1,10 @@
-package com.hamitmizrak;public class _1_WhatIsJava {
+package com.hamitmizrak;
+
+public class _1_WhatIsJava {
+
+    // Single Comment
+    /* Multiple Comment*/
+    public static void main(String[] args) {
+        System.out.println("Merhabalar Nasılsınız");
+    }
 }
