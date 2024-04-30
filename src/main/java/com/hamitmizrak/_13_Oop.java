879773b6f6db35f6feed82fec6a5ffb705885782
Hamit Mizrak <hamitmizrak@gmail.com>
2024-04-30 11:45:52 +0300

    List-Set-Map

---
+++ src/main/java/com/hamitmizrak/_13_Oop.java
@@ -0,0 +1,11 @@
+package com.hamitmizrak;
+
+public class _13_Oop {
+
+    public static void main(String[] args)  {
+        // OOP
+        // Inheritance
+        // Abstract
+        // interface
+    }
+}
